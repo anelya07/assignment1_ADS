56c15496dee643692ea9e53275056acb35a72e64
anelya07 <253652@astanait.edu.kz>
2026-03-17 19:04:36 +0500

    task 7

--- src/Main.java
+++ src/Main.java
@@ -110,7 +110,7 @@ class Main {
 
     }
 }
-*/
+
 
 //№6
 import java.util.Scanner;
@@ -144,9 +144,47 @@ class Main {
     }
 }
 
+*/
 
+//№7
+import java.util.Scanner;
 
+class Main {
+    public static void reverse(int arr[], int i) {
+        if (i == arr.length/2) {
+            return;
+        }
+        else {
+            int j = arr.length - i - 1;
+            int  temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            reverse(arr, i + 1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Size of array: ");
+        int size = sc.nextInt();
 
+        int[] arr = new int[size];
+        System.out.println("Enter " + size + " numbers:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array: ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        reverse(arr, 0);
+        System.out.println("\nReversed array: ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
 
 
 
