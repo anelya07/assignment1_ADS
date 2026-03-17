085c8a2a65ad52c8666f32c741f4ca21dd4aa50e
anelya07 <253652@astanait.edu.kz>
2026-03-17 11:57:15 +0500

    task 2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+/* #1
 class Main {
     public static int printDigit(int n) {
         if (n < 10) {
@@ -13,5 +14,26 @@ class Main {
     }
     public static void main(String[] args) {
         printDigit(5481);
+    }
+}
+*/
+
+import java.util.Scanner;
+
+class Main {
+    public static int summa(int n, int arr[]) {
+        if (n == arr.length) {
+            return 0;
+        }
+        else {
+            return arr[n] + summa(n+1, arr);
+        }
+
+    }
+    public static void main(String[] args) {
+        double num = 4;
+        int array[] = {3,2,4,1};
+        System.out.println(summa(0, array)/num);
+
     }
 }
